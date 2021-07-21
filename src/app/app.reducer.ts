3ca230bdf6d7d3442837db7db83cc4083d3a79d5
TunrayoIlawole <tunrayoilawole99@gmail.com>
2021-07-21 16:04:46 +0100

    create app state

---
+++ src/app/app.reducer.ts
@@ -0,0 +1,17 @@
+import { combineReducers, Reducer } from "redux";
+import { ThreadsReducer, ThreadsState } from "./thread/threads.reducer";
+import { UsersReducer, UsersState } from "./user/users.reducer";
+
+
+// An interface that holds all the states in the app
+export interface AppState {
+    users: UsersState;
+    threads: ThreadsState;
+}
+
+const rootReducer: Reducer<AppState> = combineReducers<AppState>({
+    users: UsersReducer,
+    threads: ThreadsReducer
+});
+
+export default rootReducer;
---
+++ src/app/app.store.ts
@@ -0,0 +1,19 @@
+import { InjectionToken } from '@angular/core'
+import { createStore, Store, compose, StoreEnhancer, Action } from 'redux';
+import { AppState, default as reducer } from './app.reducer';
+
+export const AppStore = new InjectionToken('App.store');
+
+const devtools: StoreEnhancer<AppState> = 
+    window['__REDUX_DEVTOOLS_EXTENSION__']? window['__REDUX_DEVTOOLS_EXTENSION__']() : f => f;
+
+export function createAppStore(): Store<AppState> {
+    return createStore<AppState, Action<any>, unknown, unknown>(
+        reducer,
+        compose(devtools)
+    );
+}
+
+export const appStoreProviders = [
+    { provide: AppStore, useFactory: createAppStore }
+];
